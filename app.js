ebffce4f5b2dd65a7a5665116e58a4102671258f
blackzid <blackzidd@gmail.com>
2017-02-19 05:56:13 +0800

    re

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.io = io;
+
 var home = require('./routes/index');
-var foundation = require('./routes/foundation');
+var projects = require('./routes/projects');
 var member = require('./routes/member');
 var donee = require('./routes/donee');
 
-var app = express();
+var routes = require('./routes/controller.js');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,36 +30,53 @@ app.set('view options', {
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('123456789'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', home);
-app.use('/foundation', foundation);
-app.use('/member', member);
-app.use('/donee', donee); 
 
 
+
+
+// app.use('/', home);
+// app.use('/projects', projects);
+// app.use('/member', member);
+// app.use('/donee', donee); 
+app.get('/', routes.index);
+app.get('/projects', routes.projects);
+app.get('/projects/create', routes.createContract);
+app.post('/projects/create', routes.doCreateContract);
+app.get('/projects/:projectAddress', routes.project);
+app.get('/projects/:projectAddress/manage', routes.manageProject);
+app.post('/projects/:projectAddress/manage', routes.transferEther);
+app.get('/donees', routes.donees)
+app.get('/donee/create', routes.createDonee);
+app.post('/donee/create', routes.doCreateDonee);
+app.get('/donee/:doneeAddress', routes.donee);
+
+app.get('/login', routes.member);
+app.post('/login', routes.doLogin);
+app.get('/logout', routes.logout);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+server.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
 })
 
 module.exports = app;
--- routes/member.js
+++ routes/member.js
@@ -2,7 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('member');
+	
+  res.render('member',{
+  	u_error:"",
+  	p_error:""
+  });
 });
+router.post('/', function(req, res) {
+    if (req.body['username'] == "") {
+        res.render("member",{
+        	u_error:"帳號空白或錯誤",
+        	p_error:""
+        });
+    } else if (req.body['password'] == "") {
+        res.render("member",{
+        	p_error:"密碼空白或錯誤",
+        	u_error:""
+        });
+    } else {
+    	res.cookie('admin', req.body['username'], { path: '/projects', signed: true});		
+    	res.cookie('password', req.body['password'], { path: '/projects', signed: true});		
 
+        res.redirect("/projects")
+    }
+})
 module.exports = router;
